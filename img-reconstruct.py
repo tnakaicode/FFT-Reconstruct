e73a7924cd98e9db1330b5cc31c07627a14730a3
Taku Nakai <t.nakai.office@gmail.com>
2019-09-02 20:09:42 +0900

    init main

--- img-reconstruct.py
+++ img-reconstruct.py
@@ -1,71 +1,86 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
+import time
+import os
 import numpy.fft as fft
 import scipy.ndimage as nd
 import scipy.misc as misc
 import cv2
 from PIL import Image
 from matplotlib import image
+from linecache import getline, clearcache
+from optparse import OptionParser
 
-# Read in source image
-source = image.imread("./test_png/einstein.bmp")
 
-# Pad image to simulate oversampling
-pad_len = len(source)
-padded = np.pad(source, ((pad_len, pad_len), (pad_len, pad_len)), 'constant',
-                constant_values=((0, 0), (0, 0)))
+if __name__ == '__main__':
+    argvs = sys.argv
+    parser = OptionParser()
+    parser.add_option("--dir", dest="dir", default="./test_png/")
+    parser.add_option("--name", dest="name", default="einstein")
+    opt, argc = parser.parse_args(argvs)
+    print(argc, opt)
 
-ft = fft.fft2(padded)
+    # Read in source image
+    bmpfile = opt.dir + opt.name
+    source = image.imread(bmpfile + ".bmp")
 
-# simulate diffraction pattern
-diffract = np.abs(ft)
+    # Pad image to simulate oversampling
+    pad_len = len(source)
+    padded = np.pad(source, ((pad_len, pad_len), (pad_len, pad_len)),
+                    'constant', constant_values=((0, 0), (0, 0)))
 
-l = len(padded)
+    ft = fft.fft2(padded)
 
-# keep track of where the image is vs the padding
-mask = np.ones((pad_len+2, pad_len+2))
-mask = np.pad(mask, ((pad_len-1, pad_len-1), (pad_len-1, pad_len-1)), 'constant',
-              constant_values=((0, 0), (0, 0)))
+    # simulate diffraction pattern
+    diffract = np.abs(ft)
 
-# Initial guess using random phase info
-guess = diffract * np.exp(1j * np.random.rand(l, l) * 2 * np.pi)
+    l = len(padded)
 
-# number of iterations
-r = 1001
+    # keep track of where the image is vs the padding
+    mask = np.ones((pad_len+2, pad_len+2))
+    mask = np.pad(mask, ((pad_len-1, pad_len-1), (pad_len-1, pad_len-1)), 'constant',
+                  constant_values=((0, 0), (0, 0)))
 
-# step size parameter
-beta = 0.8
+    # Initial guess using random phase info
+    guess = diffract * np.exp(1j * np.random.rand(l, l) * 2 * np.pi)
 
-# previous result
-prev = None
-for s in range(0, r):
-    # apply fourier domain constraints
-    update = diffract * np.exp(1j * np.angle(guess))
+    # number of iterations
+    r = 101
 
-    inv = fft.ifft2(update)
-    inv = np.real(inv)
-    if prev is None:
-        prev = inv
+    # step size parameter
+    beta = 0.8
 
-    # apply real-space constraints
-    temp = inv
-    for i in range(0, l):
-        for j in range(0, l):
-            # image region must be positive
-            if inv[i, j] < 0 and mask[i, j] == 1:
-                inv[i, j] = prev[i, j] - beta*inv[i, j]
-            # push support region intensity toward zero
-            if mask[i, j] == 0:
-                inv[i, j] = prev[i, j] - beta*inv[i, j]
+    # previous result
+    prev = None
+    for s in range(0, r):
+        # apply fourier domain constraints
+        update = diffract * np.exp(1j * np.angle(guess))
 
-    prev = temp
+        inv = fft.ifft2(update)
+        inv = np.real(inv)
+        if prev is None:
+            prev = inv
 
-    guess = fft.fft2(inv)
+        # apply real-space constraints
+        temp = inv
+        for i in range(0, l):
+            for j in range(0, l):
+                # image region must be positive
+                if inv[i, j] < 0 and mask[i, j] == 1:
+                    inv[i, j] = prev[i, j] - beta*inv[i, j]
+                # push support region intensity toward zero
+                if mask[i, j] == 0:
+                    inv[i, j] = prev[i, j] - beta*inv[i, j]
 
-    # save an image of the progress
+        prev = temp
 
-    if s % 10 == 0:
-        plt.figure()
-        plt.imshow(prev)
-        plt.savefig("./test_png/" + str(s)+".png")
-        print(s)
+        guess = fft.fft2(inv)
+
+        # save an image of the progress
+
+        if s % 10 == 0:
+            plt.figure()
+            plt.imshow(prev)
+            plt.savefig(bmpfile + str(s)+".png")
+            print(s)
