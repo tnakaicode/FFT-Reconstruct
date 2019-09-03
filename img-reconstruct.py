5b7bbb21b080d067439c8a6a468ff4e8fb882b60
Taku Nakai <t.nakai.office@gmail.com>
2019-09-03 23:59:35 +0900

    fix

--- img-reconstruct.py
+++ img-reconstruct.py
@@ -20,18 +20,18 @@ class ImageRec (object):
         self.bmp = self.dir + name
         self.dat = image.imread(self.bmp + ".bmp")
         self.dat_shp = self.dat.shape
+        self.dat_shp_m = (self.dat_shp[0] - 1, self.dat_shp[1] - 1)
+        self.dat_shp_p = (self.dat_shp[0] + 2, self.dat_shp[1] + 2)
 
         # padded data
         # (150, 150) -> (450, 450)
         self.dat_pad = np.pad(self.dat, (self.dat_shp, self.dat_shp),
                               'constant', constant_values=((0, 0), (0, 0)))
         self.pad_shp = self.dat_pad.shape
-        self.pad_shp_m = (self.pad_shp[0] - 1, self.pad_shp[1] - 1)
-        self.pad_shp_p = (self.pad_shp[0] + 2, self.pad_shp[1] + 2)
 
         # mask
-        self.mask = np.ones(self.pad_shp_p)
-        self.mask = np.pad(self.mask, (self.pad_shp_m, self.pad_shp_m),
+        self.mask = np.ones(self.dat_shp_p)
+        self.mask = np.pad(self.mask, (self.dat_shp_m, self.dat_shp_m),
                            'constant', constant_values=((0, 0), (0, 0)))
 
         # 2D-FFT for padded data
@@ -41,49 +41,46 @@ class ImageRec (object):
         # Initial Phase in random
         self.phas = np.random.rand(*self.pad_shp) * 2 * np.pi
         self.pad_cmp = self.pad_abs * np.exp(1j * self.phas)
+        self.pad_inv = fft.ifft2(self.pad_cmp)
 
         # number of iterations
-        r = 101
+        r = 501
         # step size parameter
-        beta = 0.8
+        beta = 0.1
 
-        self.prev = None
+        self.prev = np.real(self.pad_inv)
         for s in range(0, r):
             # Generate complex data
             # ampl: original ampli data
             # phas: previous phase data
-            self.pad_cmp = self.pad_abs * np.exp(1j * np.angle(self.phas))
+            self.pad_cmp = self.pad_abs * np.exp(1j * np.angle(self.pad_cmp))
 
             self.pad_inv = fft.ifft2(self.pad_cmp)
-            self.pad_inv = np.real(self.pad_inv)
+            self.pad_rel = np.real(self.pad_inv)
             if self.prev is None:
-                self.prev = self.pad_inv.copy()
+                self.prev = np.real(self.pad_inv)
 
             # apply real-space constraints
-            temp = self.pad_inv
-            for (i, j), val in np.ndenumerate(self.pad_inv):
+            for (i, j), val in np.ndenumerate(self.pad_rel):
                 # image region must be positive
                 if self.mask[i, j] == 1 and val < 0:
-                    self.pad_inv[i, j] = self.prev[i, j] - beta * val
+                    self.pad_rel[i, j] = self.prev[i, j] - beta * val
                 # push support region intensity toward zero
                 if self.mask[i, j] == 0:
-                    self.pad_inv[i, j] = self.prev[i, j] - beta * val
-                else:
-                    self.pad_inv[i, j] = 0
-            
-            self.prev = temp
-            self.phas = fft.fft2(self.pad_inv)
+                    self.pad_rel[i, j] = self.prev[i, j] - beta * val
+
+            self.prev = np.real(self.pad_inv)
+            self.pad_cmp = fft.fft2(self.pad_rel)
 
             # save an image of the progress
             if s % 10 == 0:
                 plt.figure()
                 plt.imshow(self.prev)
                 plt.colorbar()
-                plt.savefig(self.dir + str(s) + ".png")
+                plt.savefig(self.dir + "{0:04d}.png".format(s))
                 print(time.ctime(time.time()), s)
 
             if s % 100 == 0:
-                plt.savefig(self.dir + str(s) + ".png")
                 np.savetxt(self.dir + str(s) + ".txt", self.prev)
 
 
